5784972094591d68960258467947ce760d6b7a96
wilfriedNkeng <Nkengloic@gmail.com>
2025-08-12 14:54:38 +0200

    added a phone widget to display phone number on the side of screen

---
+++ src/Components/PhoneWidget.jsx
@@ -0,0 +1,20 @@
+import { Phone } from 'lucide-react';
+
+const PhoneWidget = () => {
+  return (
+    <div>
+      <div className="fixed right-0 top-1/2 transform -translate-y-1/2 z-50">
+        <div className="transform -rotate-90">
+            <span className="text-white tracking-widest">
+                +27 67 769 4520
+            </span>
+        </div>
+        <div onClick={() => window.open('tel:+27677624520', '_self')}>
+            <Phone />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PhoneWidget;
--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,13 +1,19 @@
 import ContentArea from '../Components/ContentArea';
 import main from '../assets/background/main.jpg';
 import ParticlesComponent from '../Components/ParticlesComponent';
+import PhoneWidget from '../Components/PhoneWidget';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 const Index = () => {
+
+
+
   return (
     <div>
       <ParticlesComponent />
       <ContentArea backgroundImg={main}>
-        
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <PhoneWidget />
       </ContentArea>      
     </div>
   );
