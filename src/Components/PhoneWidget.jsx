340a258fc9cb04e9ef924889a96c880da4f578d6
wilfriedNkeng <Nkengloic@gmail.com>
2025-08-13 22:03:03 +0200

    worked on homepage call to action button

--- src/Components/PhoneWidget.jsx
+++ src/Components/PhoneWidget.jsx
@@ -3,7 +3,7 @@ import { Phone } from 'lucide-react';
 const PhoneWidget = () => {
   return (
     <div>
-      <div className="fixed right-0 top-1/2 transform -translate-y-1/2 z-50">
+      <div className="fixed right-0 top-1/2 transform -translate-y-1/2">
         <div className="transform -rotate-90">
             <span className="text-white tracking-widest">
                 +27 67 769 4520
